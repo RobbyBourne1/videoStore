04059c5cd136cd59c41292ae4bf82f293a091969
robbybourne1 <robbybourne1@gmail.com>
2017-08-29 11:23:05 -0400

    Controllers Initialized

---
+++ videoStore/Controllers/RentalRecordController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using videoStore.Models;
+
+namespace videoStore.Controllers
+{
+    public class RentalRecordController : Controller
+    {
+    }
+}

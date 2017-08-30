b2bc2db650153cb8736cd5ee79bc0a89886e303c
robbybourne1 <robbybourne1@gmail.com>
2017-08-30 13:35:42 -0400

    Created Props for viewmodel

---
+++ videoStore/ViewModels/MovieViewModel.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace videoStore.Models
+{
+    public class MovieViewModel
+    {
+      public List<GenreModel> GenreModel { get; set; }
+      public List<MovieModel> MovieModel { get; set; }
+    }
+
+    
+}

d57ac0615a3b47e50145452d0c0ac761abf30e43
robbybourne1 <robbybourne1@gmail.com>
2017-08-30 11:49:11 -0400

    Movies and Description Populating

--- videoStore/Controllers/RentalRecordController.cs
+++ videoStore/Controllers/RentalRecordController.cs
@@ -4,15 +4,23 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using videoStore.DataContext;
 using videoStore.Models;
 
 namespace videoStore.Controllers
 {
     public class RentalRecordController : Controller
     {
+        private readonly VideoDBContext _context;
+
+        public RentalRecordController(VideoDBContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var CurrentMovies = _context.Movies.ToList();
+            return View(CurrentMovies);
         }
         public IActionResult Create()
         {

fdeeddae6902ad552782efefe4e03e8059b62dd3
robbybourne1 <robbybourne1@gmail.com>
2017-08-31 16:40:22 -0400

    Cool Stuff

--- videoStore/Services/MovieService.cs
+++ videoStore/Services/MovieService.cs
@@ -10,7 +10,8 @@ using videoStore.Models;
 namespace videoStore
 {
     class MovieService
-    {   private readonly VideoDBContext _context;
+    {
+        private readonly VideoDBContext _context;
 
         public MovieService(VideoDBContext context)
         {
@@ -44,6 +45,8 @@ namespace videoStore
             .Select(s => new RentalRecordViewModel(s));
         }
 
+        private static Func<RentalRecordModel, bool> hasDefaultDate => w => w.ReturnDate == default(DateTime);
+
         public IEnumerable<RentalRecordViewModel> GetOverdueRecords()
         {
             var customerInfo = _context.Customers;
@@ -51,19 +54,21 @@ namespace videoStore
             var allRecords = _context.RentalRecords;
             var today = DateTime.Today;
 
-            return allRecords.Where(a => a.ReturnDate == default(DateTime))
-            .Where(t => t.DueDate.CompareTo(today)<0)
-            .Include(m => m.MovieModel)
-            .Include(c => c.CustomerModel)
-            .Select(s => new RentalRecordViewModel(s));
+            return allRecords
+                .Include(m => m.MovieModel)
+                .Include(c => c.CustomerModel)
+                .Where(hasDefaultDate)
+                .Where(t => t.DueDate.CompareTo(today) < 0)
+                .Select(s => new RentalRecordViewModel(s));
         }
 
         public IEnumerable<RentalRecordViewModel> GetCheckedOutRecords()
         {
             var RentalRecord = _context.RentalRecords;
-            return RentalRecord.Include(i => i.MovieModel)
-            .Include(i => i.CustomerModel).Where(w => w.ReturnDate == default(DateTime))
-            .Select(s => new RentalRecordViewModel(s));
+            return RentalRecord
+                .Include(i => i.MovieModel)
+                .Include(i => i.CustomerModel).Where(hasDefaultDate)
+                .Select(s => new RentalRecordViewModel(s));
         }
     }
 }

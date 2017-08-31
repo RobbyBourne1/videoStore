8d4bdb9417f32f7298cd98d53058d4717d843bfc
robbybourne1 <robbybourne1@gmail.com>
2017-08-31 15:05:45 -0400

    Everything works except for returning movies

--- videoStore/Controllers/CustomerController.cs
+++ videoStore/Controllers/CustomerController.cs
@@ -1,26 +1,153 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using videoStore.DataContext;
 using videoStore.Models;
 
 namespace videoStore.Controllers
 {
     public class CustomerController : Controller
     {
-        public IActionResult Index()
+        private readonly VideoDBContext _context;
+
+        public CustomerController(VideoDBContext context)
         {
-            return View();
+            _context = context;
         }
+
+        // GET: Customer
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Customers.ToListAsync());
+        }
+
+        // GET: Customer/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customerModel = await _context.Customers
+                .SingleOrDefaultAsync(m => m.CustomerID == id);
+            if (customerModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(customerModel);
+        }
+
+        // GET: Customer/Create
         public IActionResult Create()
         {
             return View();
         }
-        public IActionResult Delete()
+
+        // POST: Customer/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CustomerID,CustomerName,CustomerPhoneNumber")] CustomerModel customerModel)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                _context.Add(customerModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customerModel);
+        }
+
+        // GET: Customer/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customerModel = await _context.Customers.SingleOrDefaultAsync(m => m.CustomerID == id);
+            if (customerModel == null)
+            {
+                return NotFound();
+            }
+            return View(customerModel);
+        }
+
+        // POST: Customer/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CustomerID,CustomerName,CustomerPhoneNumber")] CustomerModel customerModel)
+        {
+            if (id != customerModel.CustomerID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customerModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerModelExists(customerModel.CustomerID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customerModel);
+        }
+
+        // GET: Customer/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customerModel = await _context.Customers
+                .SingleOrDefaultAsync(m => m.CustomerID == id);
+            if (customerModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(customerModel);
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var customerModel = await _context.Customers.SingleOrDefaultAsync(m => m.CustomerID == id);
+            _context.Customers.Remove(customerModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerModelExists(int id)
+        {
+            return _context.Customers.Any(e => e.CustomerID == id);
         }
     }
 }
